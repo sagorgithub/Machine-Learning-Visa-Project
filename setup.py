2ccb981372adb7825e468a33f2e161fbdf7d09ad
MD SAGOR HOSEN <sagorhosenrased@gmail.com>
2025-08-21 00:37:50 +0600

    Folder and requirements.txt file setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="MD SAGOR HOSEN",
+    author_email="sagorhosenrased@gmail.com",
+    packages=find_packages()
+)
